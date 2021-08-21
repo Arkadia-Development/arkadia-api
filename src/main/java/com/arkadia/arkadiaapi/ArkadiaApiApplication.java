d6cbc7c64d2af6cdc7dab433bc1a01680e018e04
Deuce Black <deuce2black@gmail.com>
2021-08-21 17:29:05 -0500

    Basic API setup

---
+++ src/main/java/com/arkadia/arkadiaapi/ArkadiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.arkadia.arkadiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkadiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkadiaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arkadia/arkadiaapi/ArkadiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkadia.arkadiaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkadiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
